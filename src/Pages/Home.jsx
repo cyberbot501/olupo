011574ece448b0f4c171fbc9bedb3df3e441b5d7
cyberbot501 <softwareengineerjoycy@gmail.com>
2024-02-01 10:57:25 +0100

    help

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>I love coding</div>
+    <div>I love coding<br/>
+        Happy New Month<br/>
+        From Dev~cyberbot501
+    </div>
   )
 }
