f90c6a4510401d82ecb59939ddcc5efac1f67c99
cyberbot501 <softwareengineerjoycy@gmail.com>
2024-02-05 01:06:38 +0100

    landing page completed

--- src/Layouts/Navbar/nav.jsx
+++ src/Layouts/Navbar/nav.jsx
@@ -13,7 +13,7 @@ export default function nav() {
             <ul className='l-nav'>
                <li><a href='#'>Home</a></li>
                <li><a href='#'>About Us</a></li>
-               <li><a href='#'>Crops Calendar</a></li>
+               <li><a href='#'>Farming Calendar</a></li>
                <li><a href='#'>Services</a></li>
                <li><a href='#'>Contact Us</a></li>
             </ul>
