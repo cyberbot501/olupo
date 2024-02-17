8b4b5faf258e43b4015fd00ab4060b69f699271f
cyberbot501 <softwareengineerjoycy@gmail.com>
2024-02-17 18:28:15 +0100

    update

--- src/Componets/ContactComponent/location/location.jsx
+++ src/Componets/ContactComponent/location/location.jsx
@@ -8,7 +8,7 @@ export default function location() {
         <div className='office'>
             <h1>OUR OFFICE</h1>
             <p className='off'> 232, ogbomoso/ilorin express road, Ogbomoso,  Oyo state, Nigeria.</p>
-            <p className='ice'>find us on <a href=''> Google Map</a></p>
+            <p className='ice'>find us on <a href='https://www.google.com/maps/search/fet+lx+near+Ladoke+Akintola+University,+Oyo%2F+Ilorin+Road,+Ogbomosho/@8.16671,4.2573227,15.79z?entry=ttu'> Google Map</a></p>
         </div>
 
         <div className='m-locate'>
